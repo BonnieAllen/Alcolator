28645de1bee90c551eaf436bdf030ec92647d1e2
Bonnie <aalle.allen@gmail.com>
2016-02-27 00:35:06 -0500

    Added Wine and Whiskey Navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by King Kittenhead on 2/26/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
